90764391b7459107d02d30e01d0b3b84ba235d81
Qasim Siddiqi <qasimsiddiqi2322@gmail.com>
2025-01-23 02:06:44 +0500

    forms tab checklist

--- src/views/apps/caregiver/view/details/forms/index.tsx
+++ src/views/apps/caregiver/view/details/forms/index.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+'use client'
+import Grid from '@mui/material/Grid2'
+import React, { useState } from 'react'
+import InfoCard from '../profile/InfoCard'
+import CareGiverFormCard from './CaregiverFormCard'
+import CaregiverChecklist from './CaregiverChecklist'
 
 const Forms = () => {
-  return <div>Forms</div>
+  const [showChecklist, setShowChecklist] = useState(false)
+
+  const handleShowChecklist = () => {
+    setShowChecklist(true)
+  }
+
+  const handleShowForms = () => {
+    setShowChecklist(false)
+  }
+
+  return (
+    <Grid container spacing={0}>
+      <Grid size={{ xs: 12, sm: 4, md: 4 }}>
+        <InfoCard />
+      </Grid>
+      <Grid size={{ xs: 12, sm: 6, md: 8 }}>
+        {showChecklist ? (
+          <CaregiverChecklist onShowForms={handleShowForms} />
+        ) : (
+          <CareGiverFormCard onShowChecklist={handleShowChecklist} />
+        )}
+      </Grid>
+    </Grid>
+  )
 }
 
 export default Forms
