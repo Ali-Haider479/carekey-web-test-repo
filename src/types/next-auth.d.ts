eb1458205a8b686215c01ef15a259e0a25cb354f
Muhammad Ibrar <ibrarsh001@gmail.com>
2025-02-01 16:14:32 +0500

    next auth implementation

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,27 @@
+import "next-auth";
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface User {
+    id?: string;
+    userName?: string;
+    emailAddress?: string;
+    tenant?: string;
+    caregiver?: any;
+    userRoles?: any[];
+    backendAccessToken?: any;
+    backendAccessExpiresIn?: number;
+  }
+  interface Session {
+    user: {
+      id?: string;
+      userName?: string;
+      emailAddress?: string;
+      tenant?: string;
+      caregiver?: any;
+      userRoles?: any[];
+      backendAccessToken?: any;
+      backendAccessExpiresIn?: number;
+    } & DefaultSession["user"];
+  }
+}
---
+++ src/views/pages/auth/CareKeyLogin.tsx
@@ -0,0 +1,201 @@
+'use client'
+
+// React Imports
+import { useState } from 'react'
+
+// Next Imports
+import Link from 'next/link'
+import { useParams, useRouter } from 'next/navigation'
+
+// MUI Imports
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import InputAdornment from '@mui/material/InputAdornment'
+import Checkbox from '@mui/material/Checkbox'
+import Button from '@mui/material/Button'
+import FormControlLabel from '@mui/material/FormControlLabel'
+
+// Third-Party Imports
+import classnames from 'classnames'
+
+// Type Imports
+import type { Locale } from '@configs/i18n'
+
+// Component Imports
+import CustomTextField from '@core/components/mui/TextField'
+
+// Config Imports
+import themeConfig from '@configs/themeConfig'
+
+// Hook Imports
+import { useSettings } from '@core/hooks/useSettings'
+
+// Util Imports
+import { getLocalizedUrl } from '@/utils/i18n'
+import LoginPageSVG from '../../../../public/assets/icons/LoginPageSVG'
+import { dark } from '@mui/material/styles/createPalette'
+import CareKeyLogoDark from '@/@core/svg/LogoDark'
+import CareKeyLogoLight from '@/@core/svg/LogoLight'
+import { getSession, signIn } from 'next-auth/react'
+import { Grid2 as Grid } from '@mui/material'
+
+const CareKeyLogin = () => {
+  const router = useRouter()
+  // States
+  const [email, setEmail] = useState('')
+  const [error, setError] = useState('')
+  const [password, setPassword] = useState('')
+  const [alertOpen, setAlertOpen] = useState(false)
+  const [alertProps, setAlertProps] = useState<any>()
+  const [isPasswordShown, setIsPasswordShown] = useState(false)
+
+  // Hooks
+  const { lang: locale } = useParams()
+  const { settings } = useSettings()
+
+  const handleClickShowPassword = () => setIsPasswordShown(show => !show)
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value)
+  }
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+  }
+
+  const validateEmail = () => {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    if (!email) {
+      setError('Email is required.')
+    } else if (!emailPattern.test(email)) {
+      setError('Please enter a valid email address!.')
+    } else {
+      setError('')
+    }
+  }
+
+  const handleLoginClick = async () => {
+    console.log('Login button click values: ', email, password)
+
+    if (!email || !password) {
+      setAlertOpen(true)
+      setAlertProps({
+        message: 'Email and password are required.',
+        severity: 'error'
+      })
+      return
+    }
+
+    try {
+      const res = await signIn('credentials', {
+        redirect: false,
+        email,
+        password
+      })
+
+      console.log(res)
+
+      if (res?.status === 200) {
+        // Success Case
+        setAlertOpen(true)
+        setAlertProps({
+          message: 'Login Successful!',
+          severity: 'success'
+        })
+
+        const session = await getSession()
+        console.log('Session Login', session)
+
+        if (session?.user) {
+          router.push('/')
+        }
+      } else {
+        // Error Case
+        setAlertOpen(true)
+        const errorMessage =
+          res?.status === 401 ? `Credentials provided are not valid. Please try again.\n${res?.error}` : `${res?.error}`
+
+        setAlertProps({
+          message: errorMessage,
+          severity: 'error'
+        })
+      }
+    } catch (error) {
+      console.error('Login Error: ', error)
+      setAlertOpen(true)
+      setAlertProps({
+        message: 'An unexpected error occurred. Please try again later.',
+        severity: 'error'
+      })
+    }
+  }
+
+  return (
+    <div className='flex bs-full justify-center'>
+      <div
+        className={classnames(
+          'flex bs-full items-center justify-center flex-1 min-bs-[100dvh] relative p-6 max-md:hidden',
+          {
+            'border-ie': settings.skin === 'bordered'
+          }
+        )}
+      >
+        <LoginPageSVG />
+      </div>
+      <div className='flex justify-center items-center bs-full bg-backgroundPaper !min-is-full p-6 md:!min-is-[unset] md:p-12 md:is-[480px]'>
+        <div className='flex flex-col gap-6 is-full sm:is-auto md:is-full sm:max-is-[400px] md:max-is-[unset] mbs-11 sm:mbs-14 md:mbs-0'>
+          {dark ? <CareKeyLogoDark /> : <CareKeyLogoLight />}
+          <div className='flex flex-col gap-1'>
+            <Typography variant='h4'>{`Welcome to ${themeConfig.templateName}! 👋🏻`}</Typography>
+            <Typography>Please sign-in to your account</Typography>
+          </div>
+          <Grid className='flex flex-col gap-6'>
+            <CustomTextField
+              size='medium'
+              autoFocus
+              fullWidth
+              placeholder='Enter your email or username'
+              onChange={handleEmailChange}
+              onBlur={validateEmail}
+            />
+            {error && <Typography className='text-red-500 ml-2'>{error}</Typography>}
+            <CustomTextField
+              fullWidth
+              size='medium'
+              placeholder='············'
+              type={isPasswordShown ? 'text' : 'password'}
+              slotProps={{
+                input: {
+                  endAdornment: (
+                    <InputAdornment position='end'>
+                      <IconButton edge='end' onClick={handleClickShowPassword} onMouseDown={e => e.preventDefault()}>
+                        <i className={isPasswordShown ? 'bx-hide' : 'bx-show'} />
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }
+              }}
+              onChange={handlePasswordChange}
+            />
+            <div className='flex justify-between items-center flex-wrap gap-x-3 gap-y-1'>
+              <FormControlLabel control={<Checkbox />} label='Remember me' />
+              <Typography
+                className='text-end'
+                color='primary.main'
+                component={Link}
+                href={getLocalizedUrl('/pages/auth/forgot-password-v2', locale as Locale)}
+              >
+                Forgot password?
+              </Typography>
+            </div>
+            <Button fullWidth variant='contained' type='submit' onClick={handleLoginClick}>
+              Login
+            </Button>
+          </Grid>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CareKeyLogin
